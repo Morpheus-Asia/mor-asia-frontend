defff9567acc4986b42b415075d3c6f2953416d4
Morpheus Asia Developer <dev@morpheus.asia>
2025-04-01 18:04:59 +0800

    feat: trigger deployment

--- src/app/[locale]/metrics/page.tsx
+++ src/app/[locale]/metrics/page.tsx
@@ -50,7 +50,7 @@ export default async function Page({ params }: any) {
     { [locale]: "" }
   );
 
-  // =============== VIEWS
+  // ================ VIEWS
   return (
     <>
       <ClientSlugHandler localizedSlugs={localizedSlugs} />
